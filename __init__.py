ce0b9faaa41684d56b756c3188582561a3cb19e9
Raimon Esteve <raimon@nan-tic.com>
2017-05-26 09:59:23 +0200

    Add company and rule in carrier

--- __init__.py
+++ __init__.py
@@ -2,11 +2,12 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
+from . import carrier
 from . import party
-
 
 def register():
     Pool.register(
+        carrier.Carrier,
         party.Party,
         party.Address,
         party.PartyIdentifier,
