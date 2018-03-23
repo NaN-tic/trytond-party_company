107cbca7f59654ed44d91d0351d112541b80d0e0
Raimon Esteve <raimon@nan-tic.com>
2018-03-23 09:48:16 +0100

    Add default companies in party

--- party.py
+++ party.py
@@ -71,6 +71,10 @@ class Party:
                 PartyCompany.save(party_companies)
             table.drop_column('company')
 
+    @staticmethod
+    def default_companies():
+        return [Transaction().context.get('company')]
+
     @classmethod
     def create(cls, vlist):
         # TODO pass rule group without user 0 (root)
