9709031612b3d22a4f2d3d82d2fa65303fc5ae22
Raimon Esteve <raimon@nan-tic.com>
2018-07-11 18:33:41 +0200

    Add ir.rule in company employee

---
+++ employee.py
@@ -0,0 +1,18 @@
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
+from trytond.pool import PoolMeta
+from trytond.pyson import Eval, If
+
+__all__ = ['Employee']
+
+class Employee:
+    __metaclass__ = PoolMeta
+    __name__ = 'company.employee'
+
+    @classmethod
+    def __setup__(cls):
+        super(Employee, cls).__setup__()
+        cls.company.domain = [
+            ('id', If(Eval('context', {}).contains('company'), '=', '!='),
+                Eval('context', {}).get('company', -1)),
+            ]
