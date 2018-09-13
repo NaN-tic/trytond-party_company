294b9e566413795b167afc7b36b3060e23cc9fcb
Raimon Esteve <raimon@nan-tic.com>
2018-09-13 12:36:30 +0200

    Add a new rule in user company employee model #035369

--- employee.py
+++ employee.py
@@ -1,6 +1,6 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval, If
 
 __all__ = ['Employee']
@@ -15,3 +15,15 @@ class Employee(metaclass=PoolMeta):
             ('id', If(Eval('context', {}).contains('company'), '=', '!='),
                 Eval('context', {}).get('company', -1)),
             ]
+
+    @classmethod
+    def search(cls, args, offset=0, limit=None, order=None, count=False,
+            query=False):
+        Rule = Pool().get('ir.rule')
+        # TODO clear domain cache because when an user change company (set preferences),
+        # the new domain cache is [u'company', u'=', None] and not return
+        # employees with company context. At the moment, search employees
+        # drop domain cache (clear)
+        Rule._domain_get_cache.clear()
+        return super(Employee, cls).search(args, offset, limit, order,
+            count, query)
