9ce215bb46730477e9383368bd214f2d995ced48
Raimon Esteve <raimonesteve@gmail.com>
2022-07-01 18:03:17 +0200

    drop constraint bank_account_number_number_iban_exclude

--- bank.py
+++ bank.py
@@ -1,7 +1,8 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.model import fields
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
+from trytond.transaction import Transaction
 from . import party
 
 
@@ -21,3 +22,27 @@ class BankAccount(metaclass=PoolMeta):
     @classmethod
     def search_companies(cls, name, clause):
         return [('bank.party.companies',) + tuple(clause[1:])]
+
+
+class BankAccountNumber(metaclass=PoolMeta):
+    __name__ = 'bank.account.number'
+
+    @classmethod
+    def __register__(cls, module_name):
+        pool = Pool()
+        Module = pool.get('ir.module')
+
+        cursor = Transaction().connection.cursor()
+
+        party_companies = Module.search([
+            ('name', '=', 'party_company'),
+            ('state', '!=', 'activated'),
+            ], limit=1)
+        if party_companies:
+            cursor.execute("ALTER TABLE bank_account_number DROP CONSTRAINT "
+                "IF EXISTS bank_account_number_number_iban_exclude")
+            cursor.execute("ALTER table bank_account_number add constraint "
+                "bank_account_number_number_iban_exclude check (type in "
+                "('iban', 'other'))")
+
+        super(BankAccountNumber, cls).__register__(module_name)
