fcecbce4b9182f34ce6e3934e34ca418c00d7cd9
Raimon Esteve <raimonesteve@gmail.com>
2022-11-28 18:13:59 +0100

    Filter parties without parties and filter bank account by owner companies
    072165

--- bank.py
+++ bank.py
@@ -1,7 +1,8 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.model import fields
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
+from trytond.transaction import Transaction
 from . import party
 
 
@@ -13,6 +14,15 @@ class BankAccount(metaclass=PoolMeta):
     __name__ = 'bank.account'
     companies = fields.Function(fields.One2Many('company.company', None,
         'Companies'), 'get_companies', searcher='search_companies')
+    owners_by_companies = fields.Function(
+        fields.Many2Many('bank.account-party.party', 'account', 'owner',
+        'Owners'), 'get_owners_by_companies', setter='set_owners_by_companies')
+
+    @classmethod
+    def create(cls, vlist):
+        # allow create bank accounts from party when not select owners
+        with Transaction().set_context(_check_access=False):
+            return super(BankAccount, cls).create(vlist)
 
     def get_companies(self, name):
         if self.bank:
@@ -21,3 +31,31 @@ class BankAccount(metaclass=PoolMeta):
     @classmethod
     def search_companies(cls, name, clause):
         return [('bank.party.companies',) + tuple(clause[1:])]
+
+    @classmethod
+    def get_owners_by_companies(cls, records, name):
+        PartyCompany = Pool().get('party.company.rel')
+
+        user_company = Transaction().context.get('company')
+
+        res = dict((x.id, None) for x in records)
+        for record in records:
+            owners = []
+
+            for owner in record.owners:
+                owner_companies = [o.company for o in PartyCompany.search([
+                    ('party', '=', owner),
+                    ])]
+                if not owner_companies:
+                    owners.append(owner)
+                    continue
+                for company in owner_companies:
+                    if company.id == user_company:
+                        owners.append(owner)
+                        break
+            res[record.id] = [o.id for o in owners]
+        return res
+
+    @classmethod
+    def set_owners_by_companies(cls, records, name, value):
+        cls.write(records, {'owners': value})
